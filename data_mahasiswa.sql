1755382cbfce70a786fbbb16756c639d0b380c2c
maulanafatkhan <maulanafatkhan123@gmail.com>
2026-02-17 23:18:31 +0700

    Add files via upload

---
+++ data_mahasiswa.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 17 Feb 2026 pada 17.17
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ruang_kelas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_mahasiswa`
+--
+
+CREATE TABLE `data_mahasiswa` (
+  `aku` int(10) NOT NULL,
+  `saya` varchar(20) NOT NULL,
+  `gua` varchar(20) NOT NULL,
+  `me` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `data_mahasiswa`
+--
+ALTER TABLE `data_mahasiswa`
+  ADD PRIMARY KEY (`aku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
